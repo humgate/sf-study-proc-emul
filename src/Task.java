8b642f6d18b6e84268cf2c4e7f05959d1ddc6aea
Pavel Yanovsky <humgate@gmail.com>
2020-10-19 01:04:07 +0300

    Added Task class

---
+++ src/Task.java
@@ -0,0 +1,9 @@
+public abstract class Task {
+    private int number;
+    private String description;
+
+    public Task (){
+        number = 0;
+        description = "Default description";
+    }
+}
